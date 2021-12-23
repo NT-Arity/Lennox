8c043c9626c66b83f57e2726cc508209c0b8eaaa
complecks <aabanm2023@gmail.com>
2021-12-23 04:20:33 -0600

    Made math command take in single string input, allowing more complex calculations and a seemingly undefined amount of numbers per calculation. Incorporates mathjs.

--- commands/math.js
+++ commands/math.js
@@ -1,28 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const math = require('mathjs');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("math")
         .setDescription("Performs calculator functions.")
-        .addIntegerOption(option => option.setName("num1").setDescription("First number of the expression."))
-        .addStringOption(option => option.setName("operator").setDescription("Operation to be performed in the expression."))
-        .addIntegerOption(option => option.setName("num2").setDescription("Second number of the expression.")),
+        .addStringOption(option => option.setName("expression").setDescription("Expression to be performed.")),
     async execute(interaction) {
-        const num1 = interaction.options.getInteger("num1");
-        const operator = interaction.options.getString("operator").toLowerCase();
-        const num2 = interaction.options.getInteger("num2");
-        var result;
-
-        if (operator == "+" || operator == "plus" ||operator == "added to" ) {
-            result = num1 + num2;
-        } else if (operator == "-" || operator == "minus") {
-            result = num1 - num2;
-        } else if (operator == "*" || operator == "x" || operator == "X" || operator == "times" || operator == "multiply" || operator == "multiplied by") {
-            result = num1 * num2;
-        } else if (operator == "/" || operator == "divide" || operator == "divided by") {
-            result = num1 / num2;
+        const expression = interaction.options.getString("expression").toLowerCase();
+        try {
+            await interaction.reply(expression + " equals " + math.evaluate(expression));
+        } catch (err) {
+            await interaction.reply("An error occured during the evaluation of the input expression.");
+            console.log(err);
         }
-
-        await interaction.reply(num1 + " " + operator + " " + num2 + " is equal to " + result + ".");
     }
 }
