7663c36b8e8deffa34449a4f2d7bbba188796461
NintendoTool-Arity <nwade594@gmail.com>
2021-12-24 13:18:12 -0500

    added code lines for style

--- commands/kick.js
+++ commands/kick.js
@@ -17,7 +17,7 @@ module.exports = {
         if (!(await member).permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return interaction.reply({ content: "You don't have the permission to kick members\n\nYou need the \`KICK_MEMBERS\` perm", ephemeral: true })
 
         await interaction.guild.members.kick(user, kickReason).then(() => {
-            interaction.reply(`Successfully kicked ${user.tag} with the reason: ${kickReason ? kickReason : "N/A"}`);
+            interaction.reply(`Successfully kicked \`${user.tag}\` with the reason: \`${kickReason ? kickReason : "N/A"}\``);
         }).catch(console.error);
     }
 }
