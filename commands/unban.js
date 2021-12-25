312c74b274d4a43de5a5b60db2debac949a6894c
NintendoTool-Arity <nwade594@gmail.com>
2021-12-25 14:02:55 -0500

    New working command: /unban, new package

--- commands/unban.js
+++ commands/unban.js
@@ -8,16 +8,25 @@ module.exports = {
         .addStringOption(option => option.setName("target").setDescription("The member that needs to be unbanned."))
         .addStringOption(option => option.setName("reason").setDescription("The reason why the member is being unbanned.")),
     async execute(interaction) {
-        const userID = interaction.options.getString("target");
+        const userID = interaction.options.getInteger("target");
         const author = interaction.user.id;
         const unbanReason = interaction.options.getString("reason");
         const member = interaction.guild.members.fetch(author);
 
         if (!userID) return interaction.reply({ content: "You need a member to unban.", ephemeral: true });
         if (!(await member).permissions.has(Permissions.FLAGS.BAN_MEMBERS)) return interaction.reply({ content: "You don't have the permission to unban members\n\nYou need the \`KICK_MEMBERS\` perm", ephemeral: true })
+        if (userID.length != 18) return interaction.reply({ content: "That isn't an ID.", ephemeral: true });
 
-        interaction.guild.members.unban(`${userID}`)
-            .then(user => console.log(user.id))
-            .catch(console.error);
+        interaction.guild.bans.fetch().then(banned => {
+            let list = banned.map(ban => ban.user.id);
+
+            if (!list.includes(userID)) {
+                interaction.reply({ content: "That member isn't banned.", ephemeral: true });
+            } else {
+                interaction.guild.bans.remove(userID, unbanReason).then(user => {
+                    interaction.reply(`Successfully unbanned \`${user.tag}\` with the reason: ${unbanReason ? unbanReason : "N/A"}`);
+                })
+            }
+        })
     }
 }
