faae3ba14b55617ae442b913ca626e85cd258732
NintendoTool-Arity <nwade594@gmail.com>
2021-12-26 19:55:44 -0500

    Update on command(s): user & rr

--- commands/user.js
+++ commands/user.js
@@ -1,12 +1,59 @@
 // Please read the README.md file located in the command folder to understand how to make commands.
 
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const dateToMTH = require("../functions/evaluateDate");
+const houseCheck = require("../functions/houseOfWhat");
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("user")
         .setDescription("Replies with user information."),
     async execute(interaction) {
-        await interaction.reply(`Your tag: \`${interaction.user.tag}\`\nYour id: \`${interaction.user.id}\``);
+        var date = interaction.user.createdAt;
+        var month = dateToMTH(date.getUTCMonth());
+        var day = date.getUTCDate();
+        var year = date.getUTCFullYear();
+        var creationDate = `${month} ${day}, ${year}`;
+
+        await interaction.user.fetch().then(user => {
+
+            const embedUI = new MessageEmbed()
+            .setTitle("User Info")
+            .addFields({
+                name: "Tag & House",
+                value: `${interaction.user.tag} ${houseCheck(interaction.user)}`,
+                inline: false
+            }, {
+                name: "ID",
+                value: `${interaction.user.id}`,
+                inline: false
+            }, {
+                name: "Discord Account Creation Date",
+                value: `${creationDate}`,
+                inline: false
+            }, {
+                name: "Banner Color",
+                value: `${user.hexAccentColor ? user.hexAccentColor : "N/A"}`,
+                inline: false
+            }, {
+                name: "Bot?",
+                value: `${interaction.user.bot ? ":white_check_mark:" : ":x:"}`,
+                inline: false
+            }, {
+                name: "Banner Image",
+                value: "\u200B",
+            })
+            .setColor(`${user.hexAccentColor ? user.hexAccentColor : "#04ae70"}`)
+            .setThumbnail(`${interaction.user.displayAvatarURL({ dynamic: true })}`)
+            .setFooter(`Slash command executed by ${interaction.user.tag}`)
+            .setImage(`${interaction.user.bannerURL({ dynamic: true, size: 2048 }) ? interaction.user.bannerURL({ dynamic: true, size: 2048 }) : "https://cdn.discordapp.com/attachments/837778991066775553/924504316881014834/Untitled520_20211225222850_remastered.png"}`)
+            .setTimestamp();
+
+            interaction.reply({ embeds: [embedUI] });
+        });
+        
+
+        
     }
 }
